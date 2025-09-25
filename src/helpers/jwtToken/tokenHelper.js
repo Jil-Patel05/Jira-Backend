aa6c83aa8db4501afda745ab925d70d184a89511
Jil-Patel05 <jilpatel2105@gmail.com>
2025-09-25 13:02:32 +0530

    message

--- src/helpers/jwtToken/tokenHelper.js
+++ src/helpers/jwtToken/tokenHelper.js
@@ -1,30 +1,28 @@
 const sendToken = async (user, statusCode, res) => {
-    const token = user.generateJWTFromUser();
-    res.cookie('token', token, {
-        httpOnly: true,
-        secure: false,
-        maxAge: 24 * 60 * 60 * 1000,
-        sameSite: 'lax'
-    });
-    return res.status(statusCode).json({
-        success: true,
-        data: {
-            user: user.userName,
-            email: user.email,
-            role: user.role,
-        }
-    })
+  const token = user.generateJWTFromUser();
+  res.cookie("token", token, {
+    httpOnly: true,
+    secure: true,
+    maxAge: 24 * 60 * 60 * 1000,
+    sameSite: "lax",
+  });
+  return res.status(statusCode).json({
+    success: true,
+    data: {
+      user: user.userName,
+      email: user.email,
+      role: user.role,
+    },
+  });
 };
 
 const getAccessTokenFromHeader = (req) => {
-    const { token } = req.cookies;
-    return token;
-}
+  const { token } = req.cookies;
+  return token;
+};
 
 const isTokenIncluded = (req) => {
-    return (
-        req.cookies && req.cookies.token
-    )
-}
+  return req.cookies && req.cookies.token;
+};
 
-module.exports = { sendToken, getAccessTokenFromHeader, isTokenIncluded }
+module.exports = { sendToken, getAccessTokenFromHeader, isTokenIncluded };
